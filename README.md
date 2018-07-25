d6e744d5c74d6dbf36e7b1639b80653187f220a3
peterj330 <peter.a.jarrard@gmail.com>
2018-07-24 22:59:04 -0500

    added firebase

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RPS-Multiplayer
 Online, multiplayer Rock, Paper, Scissors
+**published at https://peterj330.github.io/RPS-Multiplayer/**
+**portfolio link: https://peterj330.github.io/Responsive-Portfolio/**
+
+
+
+
---
+++ assets/javascript/RPS.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCeLeQvdYDtelXZBuiggUSV_3RV10atCA4",
+    authDomain: "rpsmultiplayer-3fef9.firebaseapp.com",
+    databaseURL: "https://rpsmultiplayer-3fef9.firebaseio.com",
+    projectId: "rpsmultiplayer-3fef9",
+    storageBucket: "rpsmultiplayer-3fef9.appspot.com",
+    messagingSenderId: "861146032724"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  //======================================================================================================//
